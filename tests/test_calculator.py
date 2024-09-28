17c530f571d87f05fdfa4fb851867d5f3007012f
sharonekula <se376@njit.edu>
2024-09-28 06:10:34 -0400

    Part2 changes

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,10 +1,24 @@
-'''My Calculator Test'''
-from calculator import add, subtract
+'''My Calculator Test with Calculator instance'''
+import pytest
+from calculator import Calculator
 
 def test_addition():
     '''Test that addition function works '''    
-    assert add(2,2) == 4
+    assert Calculator.add(2,2) == 4
 
 def test_subtraction():
     '''Test that addition function works '''    
-    assert subtract(2,2) == 0
+    assert Calculator.subtract(2,2) == 0
+
+def test_multiply():
+    '''Test that addition function works '''    
+    assert Calculator.multiply(2,2) == 4
+
+def test_divide():
+    '''Test that addition function works '''    
+    assert Calculator.divide(2,2) == 1
+
+def test_dividebyzero():
+    '''Test that dividebyzero condition works'''
+    with pytest.raises(ValueError, match="DivisionByZero exception occured"):
+        Calculator.divide(2,0)
